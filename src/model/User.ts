e45ff2d9b54844a5a9fe3a44fd5ace5a428b25f1
Swayam Shalgar <swayamshalgar1211@gmail.com>
2024-10-16 22:23:57 +0530

    Zod validations

--- src/model/User.ts
+++ src/model/User.ts
@@ -22,7 +22,48 @@ export interface User extends Document{
     email: string;
     password: string;
     verifyCode: string;
-    varifyCodeExpiry: Date;
+    verifyCodeExpiry: Date;
+    isVerified: boolean,
     isAcceptingMessage: boolean;
     message: Message[]
 }
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required:[true, "user name is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        match: [/.+@.+\..+/, 'please use a valide email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify code expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    message: [MessageSchema]
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>("User",UserSchema))
+
+export default UserModel
